9fea71329f0095175aa10d48ae90a783f1120db8
darijavukic <darijavukicc@gmail.com>
2021-04-28 15:46:50 +0200

    Predaja zadace

---
+++ WebApp/KlijentWebApp.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="KlijentWebApp.aspx.cs" Inherits="WebApp.KlijentWebApp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Unesi ID:"></asp:Label>
+            <asp:TextBox ID="Id" runat="server" style="margin-left: 21px" Width="115px"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Search" style="margin-left: 26px" OnClick="Button1_Click" />
+            <br />
+            <br />
+            <asp:GridView ID="gvUsers" calss="table table-bordered responsive-table" AutoGenerateColumns="true" runat="server">
+
+       </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApp/WebService1.asmx.cs
@@ -0,0 +1,59 @@
+﻿using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data;
+using System.Data;
+
+namespace WebApp
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [System.Web.Services.WebMethod]
+        public DataTable getStudent(int id)
+        {
+            string connString = "SERVER=localhost" + ";" +
+                "PORT=3307;" +
+                "DATABASE=university;" +
+                "UID=root;" +
+                "PASSWORD=;";
+
+            MySqlConnection cnMySQL = new MySqlConnection(connString);
+
+            MySqlCommand cmdMySQL = cnMySQL.CreateCommand();
+
+            MySqlDataReader reader;
+
+            cmdMySQL.CommandText = "select * from students WHERE StudentId=" + id;
+
+            cnMySQL.Open();
+            
+            reader = cmdMySQL.ExecuteReader();
+           
+
+            DataTable dt = new DataTable();
+            
+
+            dt.Load(reader);
+        
+
+            cnMySQL.Close();
+            
+
+
+
+            return dt;
+        }
+    }
+}
